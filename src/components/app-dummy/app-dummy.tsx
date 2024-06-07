dabac53538a93e0b3a624b3ff0c31e0b99ac56b5
Purushottam Kumar <purushottam1619@gmail.com>
2024-06-08 02:06:28 +0530

    color variable

--- src/components/app-dummy/app-dummy.tsx
+++ src/components/app-dummy/app-dummy.tsx
@@ -15,7 +15,7 @@ export class AppDummy {
         <image-uploader-wrapper></image-uploader-wrapper> */}
 
         <h3>Donut progress loader using svg : Type 1</h3>
-        <donut-progress-loader percent={100} desktopSize={200}></donut-progress-loader>
+        <donut-progress-loader percent={80} desktopSize={200}></donut-progress-loader>
 
         {/* <h3>Donut progress loader using div : Type 2</h3>
         {this.completedStep > 0 && <donut-progress percent={67} desktopSize={250} mobileSize={150}></donut-progress>}
